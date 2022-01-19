65e67332519efafbdf03888b720868ab4a0b3523
Josh H <>
2022-01-19 14:19:33 -0700

    ISS Notifier updated, Finished Kanye Quotes and working through Quizzler.

--- Python/ISSNotifier/main.py
+++ Python/ISSNotifier/main.py
@@ -1,4 +1,27 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-print(response)
+MY_LAT = 51.5
+MY_LONG=-0.12
+
+
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+#
+# data = response.json()
+# print(data)
+
+parameters = {
+    'lat': MY_LAT,
+    'lng': MY_LONG,
+    'formatted': 0,
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data["results"]["sunrise"].split('T')[1].split(':')
+sunset = data["results"]["sunset"].split('T')[1].split(':')
+
+time_now = datetime.now()
